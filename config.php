f8b2c467a148c4c53399975e8b3f670c34f95e74
Roman <moi@roman.dell>
2023-10-25 23:03:54 +0200

    some modif

--- config.php
+++ config.php
@@ -6,11 +6,11 @@ define("CONFIG_ROUTES",[
     "contact" => "contact"
 ]);
 const DB_HOST = "localhost";
-const DB_NAME = "my_mvc_bdd";
-const DB_USER = "root";
-const DB_PASS = "root";
+const DB_NAME = "data_test";
+const DB_USER = "roman";
+const DB_PASS = "azerty";
 
 function connectDB(): PDO{
-    $db = new PDO('mysql:host='.DB_HOST.';port=3333;dbname='.DB_NAME.';charset=utf8',DB_USER,DB_PASS);
+    $db = new PDO('mysql:host='.DB_HOST.';port=3306;dbname='.DB_NAME.';charset=utf8',DB_USER,DB_PASS);
     return $db;
 }
---
+++ controllers/controller_adminadd.php
@@ -0,0 +1,12 @@
+<?php
+if(isset($_POST['submit'])){
+    $db = connectDB();
+    $sql = $db->prepare("INSERT INTO picture (title, description, src, author) VALUES (?,?,?,?)");
+    $sql -> execute([$_POST['title'],
+        $_POST['description'],
+        $_POST['src'],
+        $_POST['author']]);
+    header("Location:?page=adminlist");
+}
+
+include "./views/layout.phtml";
